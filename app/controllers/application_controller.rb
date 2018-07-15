5151a0cb1af2736e3be9275d9584bc194845d1dd
ERIC KELLY <eakelly220@gmail.com>
2018-07-15 21:15:06 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,7 +33,6 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/posts/:id' do
-    binding.pry
     @post = Post.find(params[:id])
     @post.name = params[:name]
     @post.content = params[:content]
